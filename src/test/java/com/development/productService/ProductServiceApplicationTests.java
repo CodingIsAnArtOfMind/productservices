23517cc29c17e6faa1b99e9ba1fac3ad4b7b3c7f
Raza Mustafa <34963595+CodingIsAnArtOfMind@users.noreply.github.com>
2023-09-29 01:43:05 +0530

    via upload

---
+++ src/test/java/com/development/productService/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.development.productService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	//@Test
+	void contextLoads() {
+	}
+
+}
